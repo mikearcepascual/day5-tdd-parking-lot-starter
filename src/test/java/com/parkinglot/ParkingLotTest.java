80887e067d11f4fc4bb1c24755f038f966bd498b
Mikearce Pascual <PASCUMI3@OOCL.COM>
2023-08-18 13:44:12 +0800

    feat: added implementation for park function

---
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -0,0 +1,18 @@
+package com.parkinglot;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class ParkingLotTest {
+    @Test
+    void should_return_parking_ticket_when_park_given_parking_lot_and_a_car() {
+     //given
+        ParkingLot parkingLot = new ParkingLot();
+        Car car = new Car();
+     //when
+        ParkingTicket ticket = parkingLot.park(car);
+     //then
+        assertNotNull(ticket);
+    }
+}
