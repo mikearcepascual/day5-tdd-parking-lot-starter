46fe25a41d40168ddd79ab1feab4e5b2a39c6977
Mikearce Pascual <PASCUMI3@OOCL.COM>
2023-08-18 14:21:43 +0800

    feat: added implementation for should_return_cars_with_right_tickets_when_fetch_twice_given_parking_lot_and_two_parked_cars

--- src/main/java/com/parkinglot/ParkingLot.java
+++ src/main/java/com/parkinglot/ParkingLot.java
@@ -1,14 +1,19 @@
 package com.parkinglot;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ParkingLot {
     private Car car;
+    Map<ParkingTicket,Car> cars = new HashMap<>();
 
     public ParkingTicket park(Car car) {
-        this.car = car;
-        return new ParkingTicket();
+        ParkingTicket parkingTicket = new ParkingTicket();
+        cars.put(parkingTicket, car);
+        return parkingTicket;
     }
 
     public Car fetch(ParkingTicket ticket) {
-        return this.car;
+        return cars.get(ticket);
     }
 }
--- src/test/java/com/parkinglot/ParkingLotTest.java
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -22,11 +22,26 @@ public class ParkingLotTest {
      //given
         ParkingLot parkingLot = new ParkingLot();
         Car car = new Car();
-
         ParkingTicket ticket = parkingLot.park(car);
         //when
         Car result = parkingLot.fetch(ticket);
      //then
         Assertions.assertEquals(result,car);
+    }
+    
+    @Test
+    void should_return_cars_with_right_tickets_when_fetch_twice_given_parking_lot_and_two_parked_cars() {
+    //given
+        ParkingLot parkingLot = new ParkingLot();
+        Car smallCar = new Car();
+        Car bigCar = new Car();
+        ParkingTicket smallCarTicket = parkingLot.park(smallCar);
+        ParkingTicket bigCarTicket = parkingLot.park(bigCar);
+        //when
+        Car fetchSmallCar = parkingLot.fetch(smallCarTicket);
+        Car fetchBigCar = parkingLot.fetch(bigCarTicket);
+     //then
+        Assertions.assertEquals(fetchSmallCar,smallCar);
+        Assertions.assertEquals(fetchBigCar,bigCar);
     }
 }
