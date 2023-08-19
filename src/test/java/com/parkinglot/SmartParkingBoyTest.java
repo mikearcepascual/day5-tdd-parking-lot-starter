d72590de166a9c467dcc62e9e2deb99533269be4
Mikearce Pascual <PASCUMI3@OOCL.COM>
2023-08-19 17:48:19 +0800

    test: added test for should_park_to_second_parkingLot_when_park_given_smart_parking_boy_two_parking_lots_and_a_car

--- src/test/java/com/parkinglot/SmartParkingBoyTest.java
+++ src/test/java/com/parkinglot/SmartParkingBoyTest.java
@@ -23,4 +23,21 @@ public class SmartParkingBoyTest {
         assertEquals(9, firstParkingLot.getAvailableCapacity());
         assertEquals(10, secondParkingLot.getAvailableCapacity());
     }
+
+    @Test
+    void should_park_to_second_parkingLot_when_park_given_smart_parking_boy_two_parking_lots_and_a_car() {
+        //given
+        ParkingLot firstParkingLot = new ParkingLot(2);
+        ParkingLot secondParkingLot = new ParkingLot();
+        List<ParkingLot> parkingLotList = List.of(firstParkingLot, secondParkingLot);
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLotList);
+        Car carToBeParked = new Car();
+
+        //when
+        ParkingTicket parkingTicket = smartParkingBoy.park(carToBeParked);
+        //then
+        assertNotNull(parkingTicket);
+        assertEquals(2,firstParkingLot.getAvailableCapacity());
+        assertEquals(9,secondParkingLot.getAvailableCapacity());
+    }
 }
