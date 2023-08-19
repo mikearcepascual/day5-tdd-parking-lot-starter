7879524d494aa010a9ad14748f3235bbb4a91309
Mikearce Pascual <PASCUMI3@OOCL.COM>
2023-08-19 21:17:40 +0800

    refactor: added import statements for UnrecognizedTicketException and NoAvailablePositionException

--- src/test/java/com/parkinglot/SuperParkingBoyTest.java
+++ src/test/java/com/parkinglot/SuperParkingBoyTest.java
@@ -1,5 +1,7 @@
 package com.parkinglot;
 
+import com.parkinglot.exception.NoAvailablePositionException;
+import com.parkinglot.exception.UnrecognizedTicketException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
